27d6690a5592117ba963701a95fa3581d557d2cd
saeed <saeed@DESKTOP-4PANF7S>
2024-01-02 17:31:05 +0330

    Add project files.

---
+++ Extentions/JsRuntimeExtentions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.JSInterop;
+namespace ToastNotification.Extentions
+
+{
+    public static class JsRuntimeExtentions
+    {
+        //For Success Message of an opration
+        public static ValueTask ToastrSuccess(this IJSRuntime jSRuntime,string message)
+        {
+            return jSRuntime.InvokeVoidAsync("ShowToastr", "success", message);
+        }
+        //for error message of an opration
+        public static ValueTask ToastrError(this IJSRuntime jsSRuntime,string message)
+        {
+            return jsSRuntime.InvokeVoidAsync("ShowToastr", "error", message);
+        }
+
+    }
+}
---
+++ wwwroot/js/Common.js
@@ -0,0 +1,11 @@
+﻿window.ShowToastr = (type, message) => {
+
+    toastr.options.toastClass = "toastr";
+
+    if (type === "success") {
+        toastr.success(message, "Opration Successful", { timeOut: 20000 });
+    }
+    if (type === "error") {
+        toastr.error(message, "Opration Failed", { timeOut: 20000 });
+    }
+};
